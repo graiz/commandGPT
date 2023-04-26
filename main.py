00f66eb613f36eba8ca78f4ef482b46eafd898b9
Gregory Raiz <503720+graiz@users.noreply.github.com>
2023-04-25 23:28:59 -0400

    added support for chained commands

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 from flask import Flask, request, jsonify, send_from_directory
-from flask_cors import CORS  # Import the CORS module
+from flask_cors import CORS
 import subprocess
 import os
 
 app = Flask(__name__)
-CORS(app)  # Enable CORS for the entire Flask app
+CORS(app)
 
-# Define the path to the directory containing the ai-plugin.json file
 well_known_dir = os.path.join(app.root_path, '.well-known')
 
 @app.route('/.well-known/<path:filename>')
@@ -16,19 +15,22 @@ def well_known(filename):
 @app.route('/run', methods=['POST'])
 def run_command():
     data = request.get_json()
-    command = data.get('command')
+    commands = data.get('commands')
 
-    if not command:
-        return jsonify({'error': 'No command provided'}), 400
+    if not commands:
+        return jsonify({'error': 'No commands provided'}), 400
 
-    try:
-        result = subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT)
-        # Split the output into a list of lines
-        output_lines = result.decode('utf-8').splitlines()
-        return jsonify({'output': output_lines})
-    except subprocess.CalledProcessError as e:
-        error_lines = e.output.decode('utf-8').splitlines()
-        return jsonify({'error': error_lines}), 400
+    output = {}
+    for i, command in enumerate(commands):
+        try:
+            result = subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT)
+            output_lines = result.decode('utf-8').splitlines()
+            output[f"command_{i}"] = {'output': output_lines}
+        except subprocess.CalledProcessError as e:
+            error_lines = e.output.decode('utf-8').splitlines()
+            output[f"command_{i}"] = {'error': error_lines}
+
+    return jsonify(output)
 
 if __name__ == '__main__':
     app.run(host='localhost', port=3333)
